c0ac4eb54d183fe89d892cad278c263fb1a4a3e0
Pierre AMBOISE <pierre.amboise@yahoo.fr>
2017-11-09 11:18:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+SuperNoel
+=========
+
+A Symfony project created on November 9, 2017, 11:02 am.
---
+++ src/SuperNoelBundle/SuperNoelBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SuperNoelBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SuperNoelBundle extends Bundle
+{
+}
